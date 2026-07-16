5d5e4ffb09364460fca8d4fe4e777ae131b5199c
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-16 09:35:05 +0700

    Add Indonesian SIAKAD paid availability strings

---
+++ public/availability/condition/siakadpaid/lang/id/availability_siakadpaid.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Indonesian strings for the SIAKAD paid availability condition.
+ *
+ * @package    availability_siakadpaid
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Tagihan SIAKAD lunas';
+$string['title'] = 'Tagihan SIAKAD dan prodi';
+$string['description'] = 'Wajib tagihan SIAKAD lunas dan sesuai prodi yang dipilih.';
+$string['label_prodi'] = 'Program studi';
+$string['anyprodi'] = 'Semua prodi aktif';
+$string['error_selectprodi'] = 'Pilih prodi atau semua prodi aktif.';
+$string['requires_lunas_anyprodi'] = 'Anda harus sudah melunasi tagihan SIAKAD.';
+$string['requires_lunas_prodi'] = 'Anda harus sudah melunasi tagihan SIAKAD dan terdaftar di prodi {$a}.';
+$string['requires_not_lunas_anyprodi'] = 'Anda harus belum melunasi tagihan SIAKAD.';
+$string['requires_not_lunas_prodi'] = 'Anda harus belum melunasi tagihan SIAKAD untuk prodi {$a}.';
+$string['privacy:metadata'] = 'Kondisi akses ini membaca data dummy SIAKAD untuk menentukan akses, tetapi tidak menyimpan data pribadi tambahan.';
