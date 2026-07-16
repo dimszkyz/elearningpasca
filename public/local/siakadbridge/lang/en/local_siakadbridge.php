0322fb891f010d6adc905be653d6bddb390679ea
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-16 09:33:22 +0700

    Add SIAKAD bridge language strings

---
+++ public/local/siakadbridge/lang/en/local_siakadbridge.php
@@ -0,0 +1,17 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * English strings for the SIAKAD bridge dummy plugin.
+ *
+ * @package    local_siakadbridge
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'SIAKAD bridge dummy';
+$string['privacy:metadata'] = 'The SIAKAD bridge dummy plugin stores development-only SIAKAD data inside Moodle for local integration testing.';
