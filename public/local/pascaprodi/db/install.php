5de843108bd58ad6dce7b6b9b5c5f53434a2f6cc
Dimas Rizky <schooldims@gmail.com>
2026-07-16 11:12:21 +0700

    feat(pascaprodi): set default teacher cohort settings

--- public/local/pascaprodi/db/install.php
+++ public/local/pascaprodi/db/install.php
@@ -23,6 +23,9 @@ function xmldb_local_pascaprodi_install(): void {
     set_config('enabled', 1, 'local_pascaprodi');
     set_config('updatenames', 1, 'local_pascaprodi');
     set_config('archiveondeleted', 1, 'local_pascaprodi');
+    set_config('autoenrolstudents', 1, 'local_pascaprodi');
+    set_config('autoenrolteachers', 1, 'local_pascaprodi');
     set_config('nameprefix', get_string('defaultnameprefix', 'local_pascaprodi'), 'local_pascaprodi');
+    set_config('teachernameprefix', get_string('defaultteachernameprefix', 'local_pascaprodi'), 'local_pascaprodi');
     set_config('archiveprefix', get_string('defaultarchiveprefix', 'local_pascaprodi'), 'local_pascaprodi');
 }
