8cc687ce5b46212527ce389e67d61296f6f91165
Dimas Rizky <schooldims@gmail.com>
2026-07-16 11:13:54 +0700

    feat(quiz): add SIAKAD access rule metadata

---
+++ public/mod/quiz/accessrule/siakad/version.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Version information for the SIAKAD quiz access rule.
+ *
+ * @package    quizaccess_siakad
+ * @copyright  2026
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'quizaccess_siakad';
+$plugin->version = 2026071600;
+$plugin->requires = 2026042000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '1.0.0';
+$plugin->dependencies = [
+    'local_siakaddummy' => 2026071600,
+];
