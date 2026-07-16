146a311f4a899406de31de5431dd09a1a482b65a
Dimas Rizky <schooldims@gmail.com>
2026-07-16 10:46:48 +0700

    chore(pascaprodi): bump version for cohort management fix

--- public/local/pascaprodi/version.php
+++ public/local/pascaprodi/version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_pascaprodi';
-$plugin->version = 2026071600;
+$plugin->version = 2026071601;
 $plugin->requires = 2026042000;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
