519f999c8ec0b1f95dfc67357d492682476f3c20
Dimas Rizky <schooldims@gmail.com>
2026-07-16 11:15:41 +0700

    feat(quiz): add Indonesian strings for SIAKAD access rule

---
+++ public/mod/quiz/accessrule/siakad/lang/id/quizaccess_siakad.php
@@ -0,0 +1,19 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['pluginname'] = 'Kelayakan SIAKAD';
+$string['settingsheader'] = 'Kelayakan peserta berdasarkan SIAKAD';
+$string['enabled'] = 'Aktifkan pembatasan SIAKAD';
+$string['enabled_desc'] = 'Hanya mahasiswa yang memenuhi syarat dan berasal dari program studi terpilih yang dapat memulai ujian ini.';
+$string['allowedprogrammes'] = 'Program studi peserta';
+$string['requirepayment'] = 'Wajib melunasi tagihan ujian';
+$string['requirepayment_desc'] = 'Mahasiswa harus melunasi semua tagihan aktif yang ditandai sebagai syarat ujian untuk periode yang dipilih.';
+$string['academicyear'] = 'Tahun akademik';
+$string['semester'] = 'Semester';
+$string['billtype'] = 'Jenis tagihan';
+$string['anyperiod'] = 'semua tahun akademik';
+$string['anysemester'] = 'Semua semester';
+$string['programmerequired'] = 'Pilih minimal satu program studi ketika pembatasan SIAKAD diaktifkan.';
+$string['descriptionprogrammes'] = 'Ujian ini tersedia untuk program studi: {$a}.';
+$string['descriptionpayment'] = 'Tagihan ujian harus lunas untuk tahun akademik {$a->academicyear}, semester {$a->semester}.';
+$string['privacy:metadata'] = 'Aturan akses ini menyimpan pengaturan kelayakan ujian dan ID program studi yang dipilih.';
