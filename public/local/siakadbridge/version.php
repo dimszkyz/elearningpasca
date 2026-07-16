653b4959731b857d8f5ead071a595a2a53010a47
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-16 09:33:15 +0700

    Add SIAKAD bridge plugin version

---
+++ public/local/siakadbridge/version.php
@@ -0,0 +1,22 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Version information for the SIAKAD bridge dummy plugin.
+ *
+ * @package    local_siakadbridge
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_siakadbridge';
+$plugin->version = 2026071300;
+$plugin->requires = 2026041000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
