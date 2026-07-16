c4d2e468ceb3116d324c8450df77ed7a63ecadeb
Dimas Rizky <schooldims@gmail.com>
2026-07-16 13:31:01 +0700

    feat(pascaprodi): assign users by prodi category and role

--- public/local/pascaprodi/user_role.php
+++ public/local/pascaprodi/user_role.php
@@ -7,7 +7,7 @@
 // (at your option) any later version.
 
 /**
- * Combined helper for assigning a system role and an optional cohort to a user.
+ * Combined helper for assigning a role and Prodi category access to a user.
  *
  * @package    local_pascaprodi
  * @copyright  2026
@@ -39,13 +39,45 @@ if ($mform->is_cancelled()) {
 if ($data = $mform->get_data()) {
     $userid = (int) $data->userid;
     $roleid = (int) $data->roleid;
-    $cohortid = (int) $data->cohortid;
+    $categoryids = array_values(array_filter(array_map('intval', (array) ($data->categoryids ?? []))));
     $messages = [];
 
     $user = $DB->get_record('user', ['id' => $userid, 'deleted' => 0], '*', MUST_EXIST);
+    $role = null;
+    $iscategoryrole = false;
 
     if ($roleid > 0) {
         $role = $DB->get_record('role', ['id' => $roleid], '*', MUST_EXIST);
+        $iscategoryrole = \local_pascaprodi\manager::is_category_role($role);
+    }
+
+    if ($role && $iscategoryrole && $categoryids) {
+        foreach ($categoryids as $categoryid) {
+            $category = core_course_category::get($categoryid, MUST_EXIST, true);
+            $categorycontext = context_coursecat::instance($categoryid);
+
+            if (!user_can_assign($categorycontext, $roleid)) {
+                throw new required_capability_exception($categorycontext, 'moodle/role:assign', 'nopermissions', 'error');
+            }
+
+            if (!$DB->record_exists('role_assignments', [
+                'roleid' => $roleid,
+                'contextid' => $categorycontext->id,
+                'userid' => $userid,
+            ])) {
+                role_assign($roleid, $userid, $categorycontext->id);
+                $messages[] = get_string('roleassignedcategory', 'local_pascaprodi', (object) [
+                    'role' => role_get_name($role, $categorycontext),
+                    'category' => format_string($category->name),
+                ]);
+            } else {
+                $messages[] = get_string('rolealreadyassignedcategory', 'local_pascaprodi', (object) [
+                    'role' => role_get_name($role, $categorycontext),
+                    'category' => format_string($category->name),
+                ]);
+            }
+        }
+    } else if ($role && strtolower((string) $role->shortname) !== 'student') {
         if (!user_can_assign($systemcontext, $roleid)) {
             throw new required_capability_exception($systemcontext, 'moodle/role:assign', 'nopermissions', 'error');
         }
@@ -62,19 +94,37 @@ if ($data = $mform->get_data()) {
         }
     }
 
-    if ($cohortid > 0) {
+    if ($categoryids && (!$role || !$iscategoryrole)) {
         require_capability('moodle/cohort:assign', $systemcontext);
-        $cohort = $DB->get_record('cohort', ['id' => $cohortid], '*', MUST_EXIST);
 
-        if (!$DB->record_exists('cohort_members', [
-            'cohortid' => $cohortid,
-            'userid' => $userid,
-        ])) {
-            cohort_add_member($cohortid, $userid);
-            $messages[] = get_string('cohortassigned', 'local_pascaprodi', format_string($cohort->name));
-        } else {
-            $messages[] = get_string('cohortalreadyassigned', 'local_pascaprodi', format_string($cohort->name));
+        foreach ($categoryids as $categoryid) {
+            $category = core_course_category::get($categoryid, MUST_EXIST, true);
+            $cohortid = \local_pascaprodi\manager::ensure_category_cohort($categoryid);
+
+            if (!$cohortid) {
+                $messages[] = get_string('cohortnotcreated', 'local_pascaprodi', format_string($category->name));
+                continue;
+            }
+
+            $cohort = $DB->get_record('cohort', ['id' => $cohortid], '*', MUST_EXIST);
+            if (!$DB->record_exists('cohort_members', [
+                'cohortid' => $cohortid,
+                'userid' => $userid,
+            ])) {
+                cohort_add_member($cohortid, $userid);
+                $messages[] = get_string('cohortassignedcategory', 'local_pascaprodi', (object) [
+                    'cohort' => format_string($cohort->name),
+                    'category' => format_string($category->name),
+                ]);
+            } else {
+                $messages[] = get_string('cohortalreadyassignedcategory', 'local_pascaprodi', (object) [
+                    'cohort' => format_string($cohort->name),
+                    'category' => format_string($category->name),
+                ]);
+            }
         }
+    } else if ($categoryids && $role && $iscategoryrole) {
+        $messages[] = get_string('teachercohortskipped', 'local_pascaprodi');
     }
 
     if (!$messages) {
