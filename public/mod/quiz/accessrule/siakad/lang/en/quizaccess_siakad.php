407164a2079e2401c153ab8a910f81fa97c98122
Dimas Rizky <schooldims@gmail.com>
2026-07-16 11:15:25 +0700

    feat(quiz): add English strings for SIAKAD access rule

---
+++ public/mod/quiz/accessrule/siakad/lang/en/quizaccess_siakad.php
@@ -0,0 +1,19 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+$string['pluginname'] = 'SIAKAD eligibility';
+$string['settingsheader'] = 'SIAKAD eligibility';
+$string['enabled'] = 'Enable SIAKAD restriction';
+$string['enabled_desc'] = 'Only eligible students from the selected study programmes may start this quiz.';
+$string['allowedprogrammes'] = 'Allowed study programmes';
+$string['requirepayment'] = 'Require paid exam bills';
+$string['requirepayment_desc'] = 'Students must have paid every active bill marked as an exam requirement for the selected period.';
+$string['academicyear'] = 'Academic year';
+$string['semester'] = 'Semester';
+$string['billtype'] = 'Billing type';
+$string['anyperiod'] = 'any academic year';
+$string['anysemester'] = 'Any semester';
+$string['programmerequired'] = 'Select at least one study programme when the SIAKAD restriction is enabled.';
+$string['descriptionprogrammes'] = 'This quiz is available to these study programmes: {$a}.';
+$string['descriptionpayment'] = 'Exam bills must be paid for academic year {$a->academicyear}, semester {$a->semester}.';
+$string['privacy:metadata'] = 'This access rule stores quiz eligibility settings and selected study programme identifiers.';
