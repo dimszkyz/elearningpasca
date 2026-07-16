dff9524ac0e9c2b8c898f52c8bfcaff4dfc9c552
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-16 09:34:46 +0700

    Add SIAKAD paid availability plugin version

---
+++ public/availability/condition/siakadpaid/version.php
@@ -0,0 +1,22 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Version information for the SIAKAD paid availability condition.
+ *
+ * @package    availability_siakadpaid
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'availability_siakadpaid';
+$plugin->version = 2026071300;
+$plugin->requires = 2026041000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
