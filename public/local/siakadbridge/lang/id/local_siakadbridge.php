8d0ca38e75424a397c0e450865211ae848ecb247
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-16 09:33:29 +0700

    Add Indonesian SIAKAD bridge language strings

---
+++ public/local/siakadbridge/lang/id/local_siakadbridge.php
@@ -0,0 +1,17 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Indonesian strings for the SIAKAD bridge dummy plugin.
+ *
+ * @package    local_siakadbridge
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Dummy integrasi SIAKAD';
+$string['privacy:metadata'] = 'Plugin dummy integrasi SIAKAD menyimpan data SIAKAD khusus pengembangan lokal di dalam Moodle untuk uji integrasi.';
