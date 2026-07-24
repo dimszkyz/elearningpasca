e651b29559b03a711413d7a1e4656021319e0cc2
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:13:24 +0700

    feat(quiz): add Indonesian access-rule strings

---
+++ public/mod/quiz/accessrule/siakad/lang/id/quizaccess_siakad.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Pembatasan ujian berdasarkan SIAKAD';
+$string['siakadgate'] = 'Akses SIAKAD';
+$string['targetprogrammes'] = 'Program Studi tujuan';
+$string['targetprogrammes_help'] = 'Pilih satu atau beberapa Program Studi yang boleh mengikuti ujian ini. Kosongkan agar aturan SIAKAD tidak diterapkan.';
+$string['billingperiod'] = 'Periode tagihan';
+$string['requirepaid'] = 'Mahasiswa wajib melunasi seluruh tagihan periode ini';
