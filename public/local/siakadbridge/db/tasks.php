f85303b5cff13e4bb0ec6225cdc27e96141d427a
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-24 09:36:01 +0700

    Run dedicated study-program sync every 15 minutes

--- public/local/siakadbridge/db/tasks.php
+++ public/local/siakadbridge/db/tasks.php
@@ -4,9 +4,18 @@ defined('MOODLE_INTERNAL') || die();
 
 $tasks = [
     [
-        'classname' => '\\local_siakadbridge\\task\\sync_siakad',
+        'classname' => '\\local_siakadbridge\\task\\sync_program_studies',
         'blocking' => 0,
         'minute' => '*/15',
+        'hour' => '*',
+        'day' => '*',
+        'month' => '*',
+        'dayofweek' => '*',
+    ],
+    [
+        'classname' => '\\local_siakadbridge\\task\\sync_siakad',
+        'blocking' => 0,
+        'minute' => '7-59/15',
         'hour' => '*',
         'day' => '*',
         'month' => '*',
