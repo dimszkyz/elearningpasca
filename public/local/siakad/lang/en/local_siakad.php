8b72ac294ec3d881586c364a67a56045c55155b4
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:12:33 +0700

    feat(siakad): add English language strings

---
+++ public/local/siakad/lang/en/local_siakad.php
@@ -0,0 +1,9 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Dummy SIAKAD integration';
+$string['notlinkedstudent'] = 'Your Moodle account is not linked to a SIAKAD student record.';
+$string['wrongprogramme'] = 'This exam is not assigned to your study programme.';
+$string['unpaidbill'] = 'This exam is unavailable because the bill for period {$a} is not fully paid.';
