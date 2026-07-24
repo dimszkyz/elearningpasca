c833b49704f669abd17411966717ba630a4c20bd
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:13:33 +0700

    feat(quiz): add English access-rule strings

---
+++ public/mod/quiz/accessrule/siakad/lang/en/quizaccess_siakad.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'SIAKAD exam access restriction';
+$string['siakadgate'] = 'SIAKAD access';
+$string['targetprogrammes'] = 'Target study programmes';
+$string['targetprogrammes_help'] = 'Select one or more study programmes allowed to take this exam. Leave empty to disable the SIAKAD rule.';
+$string['billingperiod'] = 'Billing period';
+$string['requirepaid'] = 'Require all bills in this period to be fully paid';
