8ab6aa6c546b2207c12983d3d96861d30165847d
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:12:42 +0700

    feat(quiz): add SIAKAD access rule plugin

---
+++ public/mod/quiz/accessrule/siakad/version.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'quizaccess_siakad';
+$plugin->version = 2026072400;
+$plugin->requires = 2026042000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
+$plugin->dependencies = ['local_siakad' => 2026072400];
