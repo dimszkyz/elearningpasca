0ecac20d72b4c79adfb47faa7830a404b357b2a3
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:12:24 +0700

    feat(siakad): add Indonesian language strings

---
+++ public/local/siakad/lang/id/local_siakad.php
@@ -0,0 +1,9 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Integrasi SIAKAD Dummy';
+$string['notlinkedstudent'] = 'Akun Moodle Anda belum terhubung dengan data mahasiswa SIAKAD.';
+$string['wrongprogramme'] = 'Ujian ini tidak ditujukan untuk Program Studi Anda.';
+$string['unpaidbill'] = 'Ujian belum dapat diakses karena tagihan periode {$a} belum lunas.';
