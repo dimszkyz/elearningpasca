8a05a8d2b3afb3448cdbea3356e8fd6469f1beb7
Dimas Rizky <schooldims@gmail.com>
2026-07-24 11:10:31 +0700

    feat(siakad): add dummy academic data plugin

---
+++ public/local/siakad/version.php
@@ -0,0 +1,10 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_siakad';
+$plugin->version = 2026072400;
+$plugin->requires = 2026042000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
