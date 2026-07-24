17353e2dd7591a0cc86a30fb7a46cadfaf829c3b
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-24 11:40:14 +0700

    Register automatic Program Studi category sync task

---
+++ public/local/pascaprodi/db/tasks.php
@@ -0,0 +1,21 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$tasks = [
+    [
+        'classname' => '\\local_pascaprodi\\task\\sync_remote_categories',
+        'blocking' => 0,
+        'minute' => '10',
+        'hour' => '*',
+        'day' => '*',
+        'month' => '*',
+        'dayofweek' => '*',
+    ],
+];
